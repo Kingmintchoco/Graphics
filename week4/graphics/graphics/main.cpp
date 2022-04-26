16ed85ce24bb19dedf7a1048f4ae55dc9a549d5d
Kingmintchoco <jrkr7@naver.com>
2022-04-26 11:59:27 +0900

    mid test

---
+++ week4/graphics/graphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  graphics
+//
+//  Created by 최서현 on 2022/04/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
